5ba05c20a1e9d086df81ad52c07851a9adfefebc
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-11-25 13:55:12 +0100

    gcvh

---
+++ script.js
@@ -0,0 +1,31 @@
+let brols = prompt("Quelle accessoire utilisez-vous (souris/clavier/ordinateur/tapis/imprimante/ondulateur)")
+
+switch (brols){
+    case "souris":
+        alert("J'utilise une souris");
+        break;
+
+    case "clavier":
+        alert("J'utilise un clavier");
+        break;
+
+    case "ordinateur":
+        alert("J'utilise un ordinateur");
+        break;
+
+    case "tapis":
+        alert("J'utilise un tapis");
+        break;
+
+    case "imprimante":
+        alert("J'utilise une imprimante");
+        break;
+
+    case "ondulateur":
+        alert("J'utilise un ondulateur");
+        break;
+
+    default :
+        alert("Brols inconnus")
+
+}
